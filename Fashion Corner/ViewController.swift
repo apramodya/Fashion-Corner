7fd430ff12848ce0337d4d7f6365e987e814bca1
apramodya <pramodyaabeysinghe@gmail.com>
2019-06-01 23:28:48 +0800

    Initial Commit

---
+++ Fashion Corner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fashion Corner
+//
+//  Created by Pramodya Abeysinghe on 1/6/19.
+//  Copyright © 2019 Pramodya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
